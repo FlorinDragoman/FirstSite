4c170a3dcc1529f43ef70b042d2a0d428e875dbd
FlorinDragoman <165983073+FlorinDragoman@users.noreply.github.com>
2024-04-06 22:13:19 +0300

    First site with design

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nemo libero inventore.
+           Quia, ratione culpa doloribus sunt eius porro facere. Qui quibusdam fuga quod, praesentium
+           natus nisi deleniti expedita nobis?</p>
+           <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
